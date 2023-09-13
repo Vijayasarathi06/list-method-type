7f835cca45b13666f20280700c9cea363c904da1
Vijayasarathi06 <141705162+Vijayasarathi06@users.noreply.github.com>
2023-09-13 13:25:16 +0530

    List Method Type

---
+++ list method Task.py
@@ -0,0 +1,85 @@
+##list1=[1,2,3]
+##temp=list1[0]
+##list1[0]=list1[1]
+##list1[1]=list1[2]
+##list1[2]=temp
+##print(list1)
+##list1=[1,2,4,5,6,7,3,4]
+##list1.remove(4)
+##print(list1)
+##list1.pop()
+##print(list1)
+##del list1[5]
+##print(list1)
+##list1.clear()
+##print(list1)
+##list1=[1,2,3,"wert",9]
+##for i in range(len(list1)):
+##               print(i,list1[i])
+##for i in list1:
+##    print(i)
+##fruits=["apple","banana","orange","Mango"]
+##newlist=[]
+##for i in fruits:
+##    if "n" in i:
+##        newlist.append(i)
+##print(newlist)
+##fruits.sort(reverse=False)
+##print(fruits)
+##fruits1=fruits
+##print(fruits1)
+##list1=[]
+##rangevalue=int(input("enter the length list"))
+##for i in range(rangevalue):
+##    a=int(input("enter the input value"))
+##    list1.append(a)
+##print(list1)
+##sum=0
+##for i in list1:
+##    sum+=i
+##print(sum)
+##odd number
+list1=[]
+b=0
+rangevalue=int(input("enter the length value"))
+for i in range(rangevalue):
+    s=int(input("enter the number"))
+    list1.append(s)
+print(list1)
+for i in list1:
+    if(list1%2==0):
+        print(list1=[i])
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
